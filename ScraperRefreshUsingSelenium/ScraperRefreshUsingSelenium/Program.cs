9e600cc321c0c80ae0fcabec2fb8365b60d95757
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-28 12:12:59 -0500

    Stocks now prints all stock details from the table

--- ScraperRefreshUsingSelenium/ScraperRefreshUsingSelenium/Program.cs
+++ ScraperRefreshUsingSelenium/ScraperRefreshUsingSelenium/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,17 +33,65 @@ namespace ScraperRefreshUsingSelenium
             chromeDriver.Url = "https://finance.yahoo.com/portfolio/p_1/view/v1";
             Console.WriteLine("In yahoo finance page");
 
+            ////var closePopup = chromeDriver.FindElementByXPath("//*[@id=\"fin - tradeit\"]/div[2]/div");
+            //var closePopup = chromeDriver.FindElementByXPath("//dialog[@id = '__dialog']/section/button");
+            //closePopup.Click();
+            //var items = chromeDriver.FindElementsByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[*]/td[*]/span/a");
+
+            //foreach(var item in items)
+            //{
+            //    Console.WriteLine("My WatchList: " + item.Text);
+            //}
+
+            ////chromeDriver.Close();
+
+
             //var closePopup = chromeDriver.FindElementByXPath("//*[@id=\"fin - tradeit\"]/div[2]/div");
             var closePopup = chromeDriver.FindElementByXPath("//dialog[@id = '__dialog']/section/button");
             closePopup.Click();
-            var items = chromeDriver.FindElementsByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[*]/td[*]/span/a");
-
-            foreach(var item in items)
+            //var items = chromeDriver.FindElementsByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[*]/td[*]/span/a").GetAttribute;
+            IWebElement list = chromeDriver.FindElementByTagName("tbody");
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> items = list.FindElements(By.TagName("tr"));
+            int count = items.Count();
+            List<Stock> result = new List<Stock>();
+            Console.WriteLine(count);
+            //loop to get details of each stock symbol
+            for (int i = 1; i <= count; i++)
             {
-                Console.WriteLine("My WatchList: " + item.ToString());
-            }
+                Console.WriteLine(i);
+                var symbol = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[1]/span/a").GetAttribute("innerText");
+                var lastprice = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[2]/span").GetAttribute("innerText");
+                var change = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[3]/span").GetAttribute("innerText");
+                var percentChange = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[4]/span").GetAttribute("innerText");
+                var currency = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[5]").GetAttribute("innerText");
+                var avgVolume = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[9]").GetAttribute("innerText");
+                var marketCap = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[" + i + "]/td[13]/span").GetAttribute("innerText");
 
-            //chromeDriver.Close();
-        }       
+                Stock stock = new Stock();
+                Console.WriteLine(symbol);
+                stock.Symbol = symbol;
+                Console.WriteLine(lastprice);
+                stock.Price = Decimal.Parse(lastprice);
+                Console.WriteLine(change);
+                stock.Change = Decimal.Parse(change);
+                Console.WriteLine(percentChange);
+                stock.PercentChange = Decimal.Parse(percentChange.Trim('%'));
+                Console.WriteLine(currency);
+                stock.Currency = currency;
+                Console.WriteLine(avgVolume);
+                stock.AverageVolume = avgVolume;
+                Console.WriteLine(marketCap);
+                stock.MarketCap = marketCap;
+                
+
+                //result.Add(stock);
+
+            }
+            
+            
+
+
+
+        }
     }
 }
